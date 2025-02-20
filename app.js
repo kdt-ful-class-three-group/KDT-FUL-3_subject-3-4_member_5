61a4819a349bec7f978334acba42114763d42db0
=son-jm <=>
2025-02-20 10:54:18 +0900

    404오류 return으로 오류 잡음

--- app.js
+++ app.js
@@ -8,23 +8,26 @@ const server = http.createServer(function (request, response) {
       const MAIN_PAGE = fs.readFileSync("index.html");
       response.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
       response.end(MAIN_PAGE);
+      return;
     } else if (request.url === "/style.css") {
       // style.css 파일 읽기
       const style = fs.readFileSync("style.css", "utf-8");
       response.statusCode = 200;
       response.setHeader("Content-Type", "text/css");
       response.end(style);
+      return;
     } else if (request.url === "/index.js") {
       // index.js 파일 읽기
       const js = fs.readFileSync("index.js", "utf-8");
       response.statusCode = 200;
       response.setHeader("Content-Type", "application/javascript");
       response.end(js);
+      return;
+    } else {
+      res.writeHead(404, { "Content-Type": "text/html; charset=utf-8" });
+      res.end("<h1>404 - 페이지를 찾을 수 없음</h1>");
+      return;
     }
-    // else {
-    //   res.writeHead(404, { "Content-Type": "text/html; charset=utf-8" });
-    //   res.end("<h1>404 - 페이지를 찾을 수 없음</h1>");
-    // }
   }
   if (request.method === "POST") {
     console.log(request.url);
@@ -34,9 +37,11 @@ const server = http.createServer(function (request, response) {
     request.on("end", function () {
       console.log("응답 잘 받음");
     });
+    return;
   } else {
     res.writeHead(404, { "Content-Type": "text/html; charset=utf-8" });
     res.end("<h1>404 - 페이지를 찾을 수 없음</h1>");
+    return;
   }
 });
 
