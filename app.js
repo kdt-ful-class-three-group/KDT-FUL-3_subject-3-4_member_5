3b9ea6db35d045c791c43133e887f6f6b4432924
=son-jm <=>
2025-02-20 10:44:29 +0900

    first

---
+++ app.js
@@ -0,0 +1,46 @@
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer(function (request, response) {
+  if (request.method === "GET") {
+    if (request.url === "/") {
+      // index.html 파일 읽기
+      const MAIN_PAGE = fs.readFileSync("index.html");
+      response.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+      response.end(MAIN_PAGE);
+    } else if (request.url === "/style.css") {
+      // style.css 파일 읽기
+      const style = fs.readFileSync("style.css", "utf-8");
+      response.statusCode = 200;
+      response.setHeader("Content-Type", "text/css");
+      response.end(style);
+    } else if (request.url === "/index.js") {
+      // index.js 파일 읽기
+      const js = fs.readFileSync("index.js", "utf-8");
+      response.statusCode = 200;
+      response.setHeader("Content-Type", "application/javascript");
+      response.end(js);
+    }
+    // else {
+    //   res.writeHead(404, { "Content-Type": "text/html; charset=utf-8" });
+    //   res.end("<h1>404 - 페이지를 찾을 수 없음</h1>");
+    // }
+  }
+  if (request.method === "POST") {
+    console.log(request.url);
+    request.on("data", function (data) {
+      console.log(data.toString());
+    });
+    request.on("end", function () {
+      console.log("응답 잘 받음");
+    });
+  } else {
+    res.writeHead(404, { "Content-Type": "text/html; charset=utf-8" });
+    res.end("<h1>404 - 페이지를 찾을 수 없음</h1>");
+  }
+});
+
+// 서버 실행 (포트 8000)
+server.listen(8000, function () {
+  console.log("서버 도는중 http://localhost:8000");
+});
