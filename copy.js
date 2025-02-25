1c34ff7b411965e66299697bc00d11f9670a8a2a
=son-jm <=>
2025-02-25 16:28:30 +0900

    /post/설명

--- copy.js
+++ copy.js
@@ -47,8 +47,10 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
     //정상 작동하면 html를 열어라
     res.end(html);
-  } else if (url === "/new" && method === "GET") {
-    // 글 작성 페이지
+  }
+  //새 글 작성 페이지(/new)을 연다
+  else if (url === "/new" && method === "GET") {
+    // 글 작성 페이지(HTML)
     const html = `
               <h1>새 글 작성</h1>
               <form method="POST" action="/create">
@@ -59,28 +61,43 @@ const server = http.createServer((req, res) => {
           `;
     res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
     res.end(html);
-  } else if (url.startsWith("/post/") && method === "GET") {
-    // 글 상세 보기
+  }
+  //   /post/는 /post/{id}형태로만 요청 한걸 처리한다.
+  // 글 상세 보기
+  else if (url.startsWith("/post/") && method === "GET") {
+    //parseInt() 함수는 문자열을 숫자(int형)으로 변경해준다.
+    //.split("/")[2]은  "/"기준으로 나누어서 배열로 만들어 준다. 그리고 [2]번째의 문자열을 반환 해준다.
+    //split로 받은 문자열을 parseInt()함수로 숫자로 변경 해준다.
+    //그리하여 postId변수에 url로 받은 주소값을 변환된값이 postId의 값이 된다.
     const postId = parseInt(url.split("/")[2]);
+    //posts는 모든 게시글을 담고 있는 배열
     const posts = loadPosts();
+    //find()함수로 posts에 있는 데이터의 몇번째의 id가 psot로 들어가는 postId와 p.id를 비교해서 대입 한다.
+    //그냥 p로 하면 가독성이 떨어져 다른 이름으로 하는게 보기는 편할거 같다.
+    //그리고 데이터가 없으면 undefined가 대입 한다.
     const post = posts.find((p) => p.id === postId);
-
+    //post의 값이 undefined가 아니라면 if문의 코드를 실행시킨다.
+    //게시글을 찾았다는 의미
     if (post) {
+      //post안에 있는 title와 content을 가져와서 html식으로 보여준다.
       const html = `
                   <h1>${post.title}</h1>
                   <p>${post.content}</p>
                   <p><small>작성일: ${new Date(
                     post.createdAt
                   ).toLocaleString()}</small></p>
-                  <a href="/">홈으로</a>
               `;
       res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
       res.end(html);
-    } else {
+    }
+    //데이터가 undefined이면 else로 가서 404다 라는 식으로 HTML을 보여준다.
+    else {
       res.writeHead(404, { "Content-Type": "text/html; charset=utf-8" });
       res.end("<h1>404 - 페이지를 찾을 수 없음</h1>");
     }
-  } else if (url === "/create" && method === "POST") {
+  }
+  //
+  else if (url === "/create" && method === "POST") {
     // 글 작성 처리
     let body = "";
     req.on("data", (chunk) => {
